073424bf9431ea95a1e270adee4d9cda20cf9305
M-Nabeegh <151343422+M-Nabeegh@users.noreply.github.com>
2026-06-19 18:02:10 +0500

    style: fix zoomed-in pioneered image responsive scaling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -85,7 +85,7 @@ const Home = () => {
                                 </Link>
                             </div>
                             <div className="pioneered-image">
-                                <img src={pioneeredImg} alt="Dr. Javed Altaf performing landmark surgery" loading="lazy" width="800" height="600" style={{ objectFit: 'cover' }} />
+                                <img src={pioneeredImg} alt="Dr. Javed Altaf performing landmark surgery" loading="lazy" style={{ width: '100%', height: 'auto', borderRadius: 'var(--radius-md)' }} />
                                 <div className="badge-overlay">First in Pakistan</div>
                             </div>
                         </div>
